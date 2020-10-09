6cf614c84c1a68eb0cf6870a44b5ba6d70fcea10
Colin Dallaba <colindallaba1@gmail.com>
2020-10-08 22:42:16 -0400

    pushing addition to SnackGroups class

--- src/main/java/com/techelevator/SnacksGroups.java
+++ src/main/java/com/techelevator/SnacksGroups.java
@@ -7,9 +7,32 @@ public class SnacksGroups {
 	private String name;
 	private Double price;
 	private String snackGrouping;
+	private int stockLeft;
+	
+	
 	//getters and setters
+	public int getStockLeft() {
+		return stockLeft;
+	}
+	public void setStockLeft(int stockLeft) {
+		this.stockLeft = stockLeft;
+	}
+	public String getSlot() {
+		return slot;
+	}
+	public String getName() {
+		return name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public String getSnackGrouping() {
+		return snackGrouping;
+	}
+	
 	
 	//constructor
+	
 	
 	
 }
