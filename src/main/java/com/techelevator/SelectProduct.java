3c15670f0cd53be1be183ef1dc819587cc6fcfb4
Christopher Martin <ChrisTMartin83@gmail.com>
2020-10-08 21:25:05 -0400

    Progress made itemFileToArray

---
+++ src/main/java/com/techelevator/SelectProduct.java
@@ -0,0 +1,23 @@
+package com.techelevator;
+// get list items
+// find specific items
+// confirm if sold out or not, if it is return to purchase menu
+// see if price is less than or equal to balance
+// dispense item print message
+// update balance, return to purchase menu
+
+public class SelectProduct {
+ 
+	// get items
+	public void checkIfSoldOut(String itemNumber) {
+		Items itemStock = new Items();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
