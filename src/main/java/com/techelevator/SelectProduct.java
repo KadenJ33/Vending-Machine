9046a233702f5c30804410108ad9725d7857e83f
Kaden Rankin <toastmystrudle33@gmail.com>
2020-10-09 21:40:05 -0400

    updated transaction method

--- src/main/java/com/techelevator/SelectProduct.java
+++ src/main/java/com/techelevator/SelectProduct.java
@@ -6,11 +6,9 @@ public class SelectProduct {
 	
 	double newMoney = MainMenu.getBalance();
 	
-	double change;
 	
-	public double getChange() {
-		return change;
-	}
+	
+	
 
 	MainMenu myMenu = new MainMenu();
 	SlotsArrayList data = new SlotsArrayList();
@@ -57,7 +55,7 @@ public class SelectProduct {
 	}
 	
 	public void printItemInfo(Slot mySlot) {
-		Double.toString(change);
+		
 		System.out.println( mySlot.getName() + " " + mySlot.getPrice() + " " + newMoney + "\n" + mySlot.getOutputMessage());
 		
 		myMenu.displayMenu();
